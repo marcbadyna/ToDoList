d36ba55c508f69644e542a7a45007bb3dd3cc8d7
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-22 08:24:20 -0400

    Included header files to main.

--- ToDoList/main.cpp
+++ ToDoList/main.cpp
@@ -6,9 +6,10 @@
 //
 
 #include <iostream>
+#include "task.h"
+#include "toDoList.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
     return 0;
 }
