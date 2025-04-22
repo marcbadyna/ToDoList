28468ca283404dacbdc924039a812c7d0495e878
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-22 10:02:08 -0400

    Implemented menu display in main and switch statement for choices.

--- ToDoList/main.cpp
+++ ToDoList/main.cpp
@@ -6,10 +6,49 @@
 //
 
 #include <iostream>
-#include "task.h"
 #include "toDoList.h"
 
 int main() {
+    ToDoList todo;
+    todo.loadFromFile();
+    
+    int choice;
+    
+    do {
+        std::cout << "\n===== TO-DO LIST MENU =====\n";
+                std::cout << "1. Add Task\n";
+                std::cout << "2. View Tasks\n";
+                std::cout << "3. Edit Task\n";
+                std::cout << "4. Delete Task\n";
+                std::cout << "5. Mark Task as Completed\n";
+                std::cout << "6. Exit\n";
+                std::cout << "Enter your choice: ";
+
+                std::cin >> choice;
+                std::cin.ignore();
+        
+        switch (choice) {
+            case 1:
+                todo.addTask();
+                break;
+            case 2:
+                todo.displayAllTasks();
+            case 3:
+                todo.updateTask();
+            case 4:
+                todo.removeTask();
+            case 5:
+                todo.markTaskComplete();
+            case 6:
+                std::cout << "Exiting...\n";
+                break;
+            default:
+                std::cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 6);
+    
+    todo.saveToFile();
+    std::cout << "Goodbye!\n";
     
     return 0;
 }
