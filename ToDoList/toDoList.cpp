dd80b334524c20bd7b22f9fe470cedd39fd65d8f
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-22 09:09:20 -0400

    Logic for addTask function

--- ToDoList/toDoList.cpp
+++ ToDoList/toDoList.cpp
@@ -5,4 +5,26 @@
 //  Created by Marc Badyna on 4/22/25.
 //
 
+#include <iostream>
 #include "toDoList.h"
+
+ToDoList::ToDoList() : nextId(1) {}
+
+void ToDoList::addTask() {
+    std::string title, description, dueDate;
+    
+    std::cout << "Enter task title: ";
+    std::getline(std::cin, title);
+    
+    std::cout << "Enter task description: ";
+    std::getline(std::cin, description);
+    
+    std::cout << "Enter due date (e.g., 2025-04-25): ";
+    std::getline(std::cin, dueDate);
+    
+    Task newTask(nextId, title, description, dueDate);
+    tasks.push_back(newTask);
+    ++nextId;
+    
+    std::cout << "Task added!\n";
+}
