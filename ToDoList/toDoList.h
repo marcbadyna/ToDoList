d8d7c92c8d16ca78d1e0eea87d643805000b1d7e
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-22 08:22:03 -0400

    Creted ToDoList class.

--- ToDoList/toDoList.h
+++ ToDoList/toDoList.h
@@ -5,3 +5,20 @@
 //  Created by Marc Badyna on 4/22/25.
 //
 
+#include <vector>
+#include "task.h"
+
+class ToDoList {
+private:
+    std::vector<Task> tasks;
+    int nextId;
+public:
+    ToDoList();
+    void addTask();
+    void removeTask();
+    void updateTask();
+    void markTaskComplete();
+    void displayAllTasks();
+    void saveToFile();
+    void loadFromFile();
+}
