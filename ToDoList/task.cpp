1dcda98f1f85e69d9fb6b08c7b222106efc80ef5
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-22 08:58:51 -0400

    Added logic to class functions.

--- ToDoList/task.cpp
+++ ToDoList/task.cpp
@@ -5,7 +5,27 @@
 //  Created by Marc Badyna on 4/22/25.
 //
 
+#include <iostream>
 #include "task.h"
 #include <string>
 
 Task::Task(int id, std::string title, std::string description, std::string dueDate) : id(id), title(title), description(description), dueDate(dueDate), isCompleted(false) {}
+
+void Task::markComplete() {
+    isCompleted = true;
+}
+
+void Task::editTask(std::string newTitle, std::string newDescription, std::string newDueDate) {
+    title = newTitle;
+    description = newDescription;
+    dueDate = newDueDate;
+}
+
+void Task::displayTask() {
+    std::cout << "Task ID: " << id << std::endl;
+    std::cout << "Title: " << title << std::endl;
+    std::cout << "Description: " << description << std::endl;
+    std::cout << "Due Date: " << dueDate << std::endl;
+    std::cout << "Status: " << (isCompleted ? "Completed" : "Incomplete") << std::endl;
+    std::cout << std::endl;
+}
